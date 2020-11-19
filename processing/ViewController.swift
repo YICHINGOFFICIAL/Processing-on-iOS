08d0d6400d2177008d95b8402aba647406dae5e6
“yichingofficial” <jasonkuo52@gmail.com>
2020-11-19 21:54:36 +0800

    chore(demo.html): Edit the file

--- processing/ViewController.swift
+++ processing/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+        
+    @IBOutlet weak var webView: WKWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let localFilePath = Bundle.main.url(forResource: "demo", withExtension: "html")
+        let request = URLRequest(url: localFilePath!)
+        
+        webView.scrollView.isScrollEnabled = false
+        webView.scrollView.contentInsetAdjustmentBehavior = .never
+        webView.load(request)
     }
-
-
+    
+    
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
 }
 
